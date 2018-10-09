c29f445d051ad6d3fc6a5bbeb8ab538220c7d8f9
evgeniy levakhin <evgeniy.levakhin@pcmusic.com>
2018-10-08 23:28:58 -0400

    Add project files.

---
+++ ClientScripts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientScripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
